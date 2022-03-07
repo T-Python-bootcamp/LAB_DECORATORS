acef13041e46ea9870e61dafe715e6b6ee298936
walaTuwaiq <wala.radi.rae.js@tuwaiq.edu.sa>
2022-03-07 12:21:24 +0300

    done

--- decorator.py
+++ decorator.py
@@ -1,19 +1,19 @@
 
 def check_str(func):
     def wrapper(param):
+        if isinstance(param,str):
+            if len(param) > 5:
+                return func(param)
+            else:
+                raise ValueError("the argument should have more than 5 characters.")
+        else:
+            raise ValueError("the argument should be str.")
         
-        return func(param)
     return wrapper
 
 @check_str
 def check_length(param):
-    if isinstance(param,str):
-        if len(param) > 5:
-            print("The argument is more than 5 characters.")
-        else:
-            raise ValueError("the argument should have more than 5 characters.")
-    else:
-        raise ValueError("the argument should be str")
+    print(f"[{param}] is validated!")
     
 
-check_length("hello!")
+check_length("Hello!")
