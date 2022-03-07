a6f6ddcf878b5ba08c5d6e292e342fdebe0f938c
RawanZaben <rawan.zaben.rae.js@tuwaiq.edu.sa>
2022-03-07 11:05:14 +0300

    dec

--- LAB_DECORATORS.py
+++ LAB_DECORATORS.py
@@ -1,13 +1,28 @@
+# def my_decorator(param):
+#     print("string")
+#     return param
+
+# def my_func(x:str):
+#     if len(x) >=5:
+#       print(" is more than 5 ")
+#     else:
+#         print("should have more than 5 characters ")
+
+# decorated_func = my_decorator(my_func)
+# decorated_func("jj")
+################################
 def my_decorator(param):
-    print("string")
-    return param
+    def wrapped(x):
+        if isinstance(param,str):
+            print("string")
+        return param(x)
+    return wrapped
 
-def my_func(x:str):
-    if len(x) >=5:
-      print(" is more than 5 ")
-    else:
-        print("should have more than 5 characters ")
+@my_decorator
+def strLength(x):
+   if len(x) >= 5:
+      print(" string is more than 5 characters")
+   else:
+      print("string should have more than 5 characters ")
 
-decorated_func = my_decorator(my_func)
-decorated_func("jj")
-
+strLength("jjnnn") 
