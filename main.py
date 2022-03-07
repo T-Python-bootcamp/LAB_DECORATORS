5b0bdbb8c21eefcc700618bd5479103a5ecfdeb1
Nouf112233 <Nouf.Mohamed.elq.js@tuwaiq.edu.sa>
2022-03-07 12:21:40 +0300

    add

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 def my_decorator(param):
     def wrapped(string):
-        print("check the string")
+        if type(string)==str and len(string)>5:
+           print(f"the string is {string}") 
+        else:
+            raise Exception("the argument should be str and it should have more than 5 characters")
         return param(string)
     return wrapped
     
@@ -8,10 +11,8 @@ def my_decorator(param):
 
 @my_decorator
 def checkstring(string):
-    if type(string)==str and len(string)>5:
-        print(f"the string is{string}")
-        return string
-    raise Exception("the argument should be str and it should have more than 5 characters")
+    print("check the string")
+    
 
 checkstring("saraaaa")
 
